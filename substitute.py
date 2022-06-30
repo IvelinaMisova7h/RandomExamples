6e8191e36607a1542b53730503e0e4849ca24c4a
ivelinamisova <ivelina_misova@abv.bg>
2022-06-30 15:03:36 +0300

    init

---
+++ substitute.py
@@ -0,0 +1,14 @@
+k = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+
+for num_1 in range(k, 8+1):
+    for num_2 in range(9, l-1, - 1):
+        for num_3 in range(m, 8+1):
+            for num_4 in range(9, n-1, -1):
+                if (num_1 % 2 == 0 and not num_2 % 2 == 0) and (num_3 % 2 == 0 and not num_4 % 2 == 0):
+                    if not num_1 == num_3 and not num_2 == num_4:
+                        print(f"{num_1} {num_2} - {num_3} {num_4}")
+                    else:
+                        print('Cannot change the same player.')
